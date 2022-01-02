b7091cf017461e701d20faf31333db30dfd5387f
vmfaldwntjd <75198221+vmfaldwntjd@users.noreply.github.com>
2022-01-02 16:46:00 +0900

    Algorithm

---
+++ 알고리즘 라이브러리/정렬/퀵정렬/quick_sort.py
@@ -0,0 +1,27 @@
+import time
+start = time.time()
+arr = [5, 7, 9 ,0, 3, 1, 6, 2, 4, 8]
+def quick_sort(arr, start, end):
+    if start >= end: #원소가 1개인 경우 종료
+        return 
+    pivot = start #피벗은 첫 번째 인덱스이다.
+    left = start + 1
+    right = end
+    while left <= right:
+        #피벗보다 큰 데이터를 찾을 때까지 반복(왼쪽에서 진행)
+        while left <= end and arr[left] <= arr[pivot]:
+            left += 1
+        #피벗보다 작은 데이터를 찾을 때까지 반복(오른쪽에서 진행)
+        while right > start and arr[right] >= arr[pivot]:
+            right -= 1
+        if left > right: #엇갈렸다면 작은 데이터와 피벗을 교체
+            arr[right], arr[pivot] = arr[pivot], arr[right]
+        else: #엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
+            arr[left], arr[right] = arr[right], arr[left]
+    #분할 이후 왼쪽 부분과 오른쪽 부분으로 각각 정렬 수행 
+    quick_sort(arr, start, right - 1)
+    quick_sort(arr, right + 1, end)
+quick_sort(arr, 0, len(arr)-1)
+end = time.time()
+print(end - start) #1.9788742065429688e-05
+print(arr)
