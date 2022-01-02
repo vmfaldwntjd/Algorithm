2e28c9e83d54717d67db7877432ae3161db7a011
vmfaldwntjd <75198221+vmfaldwntjd@users.noreply.github.com>
2022-01-02 10:07:22 +0900

    Algorithm

---
+++ 알고리즘 라이브러리/힙소트를 사용한 정렬(O(NlogN))/최대힙/heapsort.py
@@ -0,0 +1,15 @@
+import heapq
+
+def heapsort(iterable):
+    h = []
+    result = []
+    # 모든 원소를 차례대로 삽입
+    for value in iterable:
+        heapq.heappush(h, -value)
+    #힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+    for _ in range(len(h)):
+        result.append(-heapq.heappop(h))
+    return result
+
+result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
+print(result) #[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
