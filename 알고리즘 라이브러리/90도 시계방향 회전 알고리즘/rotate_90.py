94fa3498d87ebb06e841f96a85cd504c71adb80c
vmfaldwntjd <75198221+vmfaldwntjd@users.noreply.github.com>
2022-01-02 09:46:53 +0900

    Algorithm

---
+++ 알고리즘 라이브러리/90도 시계방향 회전 알고리즘/rotate_90.py
@@ -0,0 +1,20 @@
+''' 2차원 리스트(행렬)를 90도 회전하는 메서드'''
+def rotate_a_matrix_by_90_degree(a):
+    row_length = len(a) #행의 길이
+    column_length = len(a[0]) #열의 길이
+
+    result = [[0] * row_length for _ in range(column_length)]
+    for r in range(row_length):
+        for c in range(column_length):
+            result[c][row_length - 1 - r] = a[r][c]
+    return result
+
+#사용 예시
+a = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+]
+
+#시계 방향으로 90도 회전
+print(rotate_a_matrix_by_90_degree(a))
