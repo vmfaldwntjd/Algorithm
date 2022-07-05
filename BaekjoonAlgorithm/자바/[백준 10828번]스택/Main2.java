7e151698a1c916195ed5afb4aec1d88cb11103ed
appleJuice <home@appleJuices-MacBook-Pro.local>
2022-07-05 15:39:50 +0900

    Baekjoon

---
+++ BaekjoonAlgorithm/자바/[백준 10828번]스택/Main2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = sc.nextInt();
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int k=0; k<n; k++) {
+            String cmd = sc.next();
+            if (cmd.equals("push")) {
+                int num = Integer.parseInt(sc.next());
+                stack.push(num);
+            } else if (cmd.equals("top")) {
+                if (stack.empty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(stack.peek()+"\n");
+                }
+            } else if (cmd.equals("size")) {
+                bw.write(stack.size()+"\n");
+            } else if (cmd.equals("empty")) {
+                if (stack.empty()) {
+                    bw.write("1\n");
+                } else {
+                    bw.write("0\n");
+                }
+            } else if (cmd.equals("pop")) {
+                if (stack.empty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(stack.pop()+"\n");
+                }
+            }
+        }
+        bw.flush();
+    }
+}
