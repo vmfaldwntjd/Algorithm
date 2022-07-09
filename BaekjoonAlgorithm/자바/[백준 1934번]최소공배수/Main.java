4d84814cdec786eca1e59fc32a0425eaabc87742
appleJuice <home@appleJuices-MacBook-Pro.local>
2022-07-09 15:52:09 +0900

    Baekjoon

---
+++ BaekjoonAlgorithm/자바/[백준 1934번]최소공배수/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int g = gcd(a, b);
+            bw.write((a * b) / g + "\n");
+        }
+        bw.flush();
+    }
+}
