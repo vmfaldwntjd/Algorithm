179dccc7eaca81d1ca894fde9fbe691493aa3298
appleJuice <jinjs6@gmail.com>
2022-07-12 14:38:09 +0900

    Baekjoon

---
+++ BaekjoonAlgorithm/자바/[백준 11727번]2 x n 타일링 2/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] d = new int[1001];
+        d[1] = 1;
+        d[2] = 3;
+
+        if (n > 2) {
+            for (int i = 3; i <= n; ++i) {
+                d[i] = (d[i - 1] + d[i - 2] * 2) % 10007;
+            }
+        }
+        bw.write(d[n] + "\n");
+        bw.flush();
+    }
+}
