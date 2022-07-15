82b2db5f5e14cd6eb13961cd819465a9ebc9a5e6
appleJuice <jinjs6@gmail.com>
2022-07-15 16:23:24 +0900

    Baekjoon

---
+++ BaekjoonAlgorithm/자바/[백준 2225번]합분해/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+
+    public static long mod = 1000000000L;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+        long[][] d = new long[k + 1][n + 1];
+
+        d[0][0] = 1L;
+        for (int i = 1; i <= k; ++i) {
+            for (int j = 0; j <= n; ++j) {
+                for (int l = 0; l <= j; ++l) {
+                    d[i][j] += d[i - 1][j - l];
+                    d[i][j] %= mod;
+                }
+            }
+        }
+        bw.write(d[k][n] + "");
+        bw.flush();
+    }
+}
