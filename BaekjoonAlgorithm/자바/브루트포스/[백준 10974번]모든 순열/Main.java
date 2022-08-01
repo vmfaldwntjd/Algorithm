47094380693c45f27e56ab13e50776e4d4e8acce
appleJuice <jinjs6@gmail.com>
2022-08-01 20:36:10 +0900

    Baekjoon

---
+++ BaekjoonAlgorithm/자바/브루트포스/[백준 10974번]모든 순열/Main.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static boolean next_permutation(int[] arr, int n) {
+        int i = n - 1;
+        while (i > 0 && arr[i - 1] >= arr[i])
+            --i;
+        if (i <= 0)
+            return false;
+        int j = n - 1;
+        while (arr[j] <= arr[i - 1])
+            --j;
+        swap(arr, j, i - 1);
+        j = n - 1;
+        while (i < j)
+            swap(arr, i++, j--);
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; ++i)
+            arr[i] = i + 1;
+        do {
+            for (int i = 0; i < n; ++i)
+                bw.write(arr[i] + " ");
+            bw.write("\n");
+        } while (next_permutation(arr, n));
+        bw.flush();
+    }
+}
