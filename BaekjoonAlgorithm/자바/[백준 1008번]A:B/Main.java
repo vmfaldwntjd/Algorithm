ee6c28ef7a4347b6a1e027a06762fc1ed64a8514
appleJuice <jinjs6@gmail.com>
2022-08-04 22:22:04 +0900

    Baekjoon

---
+++ BaekjoonAlgorithm/자바/[백준 1008번]A:B/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double a = Double.parseDouble(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
+        bw.write(String.format("%.9f", a / b) + "\n");
+        bw.flush();
+    }
+}
