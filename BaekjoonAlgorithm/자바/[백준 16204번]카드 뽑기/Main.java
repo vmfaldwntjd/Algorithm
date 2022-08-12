a7f8d98f2d122f0b7f0add7abd224692c92046a2
appleJuice <jinjs6@gmail.com>
2022-08-12 23:38:46 +0900

    Baekjoon

---
+++ BaekjoonAlgorithm/자바/[백준 16204번]카드 뽑기/Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        bw.write(Math.min(m, k) + Math.min(n - k, n - m) + "\n");
+        bw.flush();
+    }
+}
